9d8cdaeeac4b4e73c99b873d17da9cf838dceea9
Murray Walker <45606870+muzwalks@users.noreply.github.com>
2019-11-21 15:21:51 +0200

    removed secret key

--- myband/settings.py
+++ myband/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=u9=aj6#fhd57bd#)ar$+1h2eif!)90zumf-hevzd1mn%!hthr'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
